7330d304ced4dc89afdd8d74e22660239ef49c22
LandShark <jseaman@pointpark.edu>
2021-03-29 13:06:43 -0400

    Testing in Firefox

--- push.js
+++ push.js
@@ -13,14 +13,15 @@ push.setVapidDetails('mailto:jseaman@pointpark.edu', vapidKeys.publickey, vapidK
 
 let sub = {
     endpoint:
-      'https://fcm.googleapis.com/fcm/send/e4f7haybxow:APA91bGFBkV2oBBuu4t1Xnz-fDXHERza2wcIz8M_j0wxJyNGyDiU6JO3T09ABxJEv6q6KvVCP7YPy8Uy65uO-lX0-8dsHpo7gfYUQWv7LgjtnYD9wDfcyAaFJUBsA1VJIjq_BXJFEe_Y',
+      'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABgY…31eFtKk7s1jTb3Eh73Uo1VVv_2YhFP4n6AvEGTQQ2ff94goM5cZsnxon6MoI',
     expirationTime: null,
     keys: {
       p256dh:
-        'BEmxDHLfaJXTntRU6xUbb_4NuJ91m-3hx0351hSKI8pY0Fsl9kiX5pLdkW3Xn6M4nofcXVbcNW0n0_p7wd-S8OY',
-      auth: 'DPwziQAufRznlCRzuzFy-w'
+        'BFXW2Gom0lQZ7yOMGYqwKsN2FYx6mrYIUgErhdPti4q8Aqx5yoKnqKxuxUDvtbaxpNT_iU-Wb4dCEvvTBJrjMOY',
+      auth: 'DiUoSdgKI_M0fNQALkDjhA'
     }
   };
   
   push.sendNotification(sub, 'test message');
+  
   
