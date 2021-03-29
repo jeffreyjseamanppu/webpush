c1c21e72c9dd3c5270e44fe0613ea657ab142a85
LandShark <jseaman@pointpark.edu>
2021-03-28 23:41:17 -0400

    Added more

--- push.js
+++ push.js
@@ -1,14 +1,12 @@
 var push = require('web-push');
 
-//let vapidKeys = push.generateVAPIDKeys();
-//console.log(vapidKeys);
 
 let vapidKeys = {
     publickey: 'BDzYfjVqZoHJdMJaThzhiYGKbkx6f5oDN4YujNwWjTdCbbZlLC2eX_OvMk2MoCDzX5dF0MqvUmyVzadT_P9mY9w',
     privatekey: 'rHaWsC7qeIHoMRwH1YTSJo3YBTc4yuEpVzg85vv-5qw'
-}
+};
 
-push.setVapidDetails('mailto:jseaman@pointpark.edu', vapidKeys.publickey, vapidKeys.privatekey)
+push.setVapidDetails('mailto:jseaman@pointpark.edu', vapidKeys.publickey, vapidKeys.privatekey);
 
 //If we were using a server and API. When we'd click the subscribe button, the end point would be provided back to us
 //We are hard coding this as if we did receive this information from the server.
@@ -25,3 +23,4 @@ let sub = {
   };
   
   push.sendNotification(sub, 'test message');
+  
