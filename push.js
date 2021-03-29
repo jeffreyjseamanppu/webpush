284fe8285d6ab4752a03f9790ab12b2c2c2abd8c
LandShark <jseaman@pointpark.edu>
2021-03-29 14:09:28 -0400

    Debugging with chrome

--- push.js
+++ push.js
@@ -11,6 +11,8 @@ push.setVapidDetails('mailto:jseaman@pointpark.edu', vapidKeys.publickey, vapidK
 //If we were using a server and API. When we'd click the subscribe button, the end point would be provided back to us
 //We are hard coding this as if we did receive this information from the server.
 
+//credentials for firefox
+/*
 let sub = {
     endpoint:
       'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABgY…31eFtKk7s1jTb3Eh73Uo1VVv_2YhFP4n6AvEGTQQ2ff94goM5cZsnxon6MoI',
@@ -21,7 +23,20 @@ let sub = {
       auth: 'DiUoSdgKI_M0fNQALkDjhA'
     }
   };
+  */
   
+  //credentials for chrome
+  let sub = {
+    endpoint:
+      '"https://fcm.googleapis.com/fcm/send/e4f7haybxow:APA91bGFBkV2oBBuu4t1Xnz-fDXHERza2wcIz8M_j0wxJyNGyDiU6JO3T09ABxJEv6q6KvVCP7YPy8Uy65uO-lX0-8dsHpo7gfYUQWv7LgjtnYD9wDfcyAaFJUBsA1VJIjq_BXJFEe_Y',
+    expirationTime: null,
+    keys: {
+      p256dh:
+        'BEmxDHLfaJXTntRU6xUbb_4NuJ91m-3hx0351hSKI8pY0Fsl9kiX5pLdkW3Xn6M4nofcXVbcNW0n0_p7wd-S8OY',
+      auth: 'DPwziQAufRznlCRzuzFy-w'
+    }
+  };
+
   push.sendNotification(sub, 'test message');
   
   
