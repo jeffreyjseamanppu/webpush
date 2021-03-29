fc61516337cd60d67d51cbcb15d700a69a330c2d
LandShark <jseaman@pointpark.edu>
2021-03-28 22:49:17 -0400

    Remove images

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('push', () => {
 self.addEventListener('push', function(e) {
     var options = {
       body: 'This notification was generated from a push!',
-      icon: 'images/example.png',
+      //icon: 'images/example.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
@@ -16,9 +16,9 @@ self.addEventListener('push', function(e) {
         {
           action: 'explore',
           title: 'Explore this new world',
-          icon: 'images/checkmark.png'
+          //icon: 'images/checkmark.png'
         },
-        { action: 'close', title: 'Close', icon: 'images/xmark.png' }
+        //{ action: 'close', title: 'Close', icon: 'images/xmark.png' }
       ]
     };
     e.waitUntil(self.registration.showNotification('Hello world!', options));
